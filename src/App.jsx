c33833982722fed09eca1c867def31c0bef55e99
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-07-07 20:37:14 +0300

    add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import { NavLink, Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Vite + React</h1>
+      <nav>
+        <NavLink to="/cosmetics_shop_vite/">Home</NavLink> {"|"}
+        <NavLink to="/cosmetics_shop_vite/contacts">Contacts</NavLink>
+      </nav>
+      <Outlet />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +27,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Contacts from "./pages/Contacts/Contacts.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/cosmetics_shop_vite/",
+    element: <App />,
+    children: [
+      {
+        path: "/cosmetics_shop_vite/",
+        element: <Home />,
+      },
+      {
+        path: "/cosmetics_shop_vite/contacts",
+        element: <Contacts />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
