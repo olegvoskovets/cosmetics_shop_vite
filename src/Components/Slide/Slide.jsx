39a0f3d3d3fdeadf8a399a2147764cb2bf2e0379
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-08-01 23:59:19 +0300

    add img

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -1,15 +1,28 @@
 import css from "./Slide.module.css";
 import img from "../../data/slide.json";
+import { useEffect, useState } from "react";
 
 const Slide = () => {
+  const [imgSlide, setImgSlide] = useState(img);
+
+  useEffect(() => {
+    let i = 0;
+    setInterval(() => {
+      if (i === img.length) {
+        i = 0;
+      }
+      setImgSlide(img[i]);
+      i += 1;
+    }, 5000);
+  }, []);
   return (
     <section className={css.slide_section}>
       <div className="container">
         <picture title="slide image">
-          <source srcSet={img[0].img_tablet} media="(max-width: 1280px)" />
-          <source srcSet={img[0].img_dectop} />
+          <source srcSet={imgSlide.img_tablet} media="(max-width: 1280px)" />
+          <source srcSet={imgSlide.img_dectop} />
           <img
-            src={img[0].img_dectop}
+            src={imgSlide.img_dectop}
             alt="image Slide"
             className={css.slide_img}
           />
